0a11d896287ce532978de24d6df389590e94dc74
comp500 <comp500@users.noreply.github.com>
2023-04-03 20:24:52 +0100

    Fix possible concurrency issue with boss event

--- src/main/java/link/infra/modvote/ui/BossEventUpdater.java
+++ src/main/java/link/infra/modvote/ui/BossEventUpdater.java
@@ -16,25 +16,29 @@ public class BossEventUpdater {
 	private static final UUID evtId = UUID.randomUUID();
 
 	public static void update(int ticks) {
-		BossHealthOverlay healthOverlay = ((GuiAccessor) Minecraft.getInstance().gui).getBossOverlay();
-		Map<UUID, LerpingBossEvent> events = ((BossHealthOverlayAccessor)healthOverlay).getEvents();
-		LerpingBossEvent evt = events.get(evtId);
-		Component text = Component.translatable("boss.modvote.reloading", ticks / 20);
-		if (evt == null) {
-			evt = new LerpingBossEvent(evtId,
-				text, 0,
-				BossEvent.BossBarColor.PURPLE,
-				BossEvent.BossBarOverlay.PROGRESS, true, true, true);
-			events.put(evtId, evt);
-		}
+		Minecraft.getInstance().execute(() -> {
+			BossHealthOverlay healthOverlay = ((GuiAccessor) Minecraft.getInstance().gui).getBossOverlay();
+			Map<UUID, LerpingBossEvent> events = ((BossHealthOverlayAccessor)healthOverlay).getEvents();
+			LerpingBossEvent evt = events.get(evtId);
+			Component text = Component.translatable("boss.modvote.reloading", ticks / 20);
+			if (evt == null) {
+				evt = new LerpingBossEvent(evtId,
+					text, 0,
+					BossEvent.BossBarColor.PURPLE,
+					BossEvent.BossBarOverlay.PROGRESS, true, true, true);
+				events.put(evtId, evt);
+			}
 
-		evt.setName(text);
-		evt.setProgress((RulesManager.UPDATE_DELAY - (float)ticks) / RulesManager.UPDATE_DELAY);
+			evt.setName(text);
+			evt.setProgress((RulesManager.UPDATE_DELAY - (float)ticks) / RulesManager.UPDATE_DELAY);
+		});
 	}
 
 	public static void clear() {
-		BossHealthOverlay healthOverlay = ((GuiAccessor) Minecraft.getInstance().gui).getBossOverlay();
-		Map<UUID, LerpingBossEvent> events = ((BossHealthOverlayAccessor)healthOverlay).getEvents();
-		events.remove(evtId);
+		Minecraft.getInstance().execute(() -> {
+			BossHealthOverlay healthOverlay = ((GuiAccessor) Minecraft.getInstance().gui).getBossOverlay();
+			Map<UUID, LerpingBossEvent> events = ((BossHealthOverlayAccessor) healthOverlay).getEvents();
+			events.remove(evtId);
+		});
 	}
 }
